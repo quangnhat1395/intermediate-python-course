4507e1d640b3c2e32df3b0da72a02917874c8bce
quangnhat1395 <truongquangnhat1395@gmail.com>
2021-10-31 01:05:13 +0700

    Update Critical Fail

--- dice_roller.py
+++ dice_roller.py
@@ -3,10 +3,13 @@ def main():
 
   dice_rolls = 2
   dice_sum = 0
-  for i in range(dice_rolls):
+  for i in range(0, dice_rolls):
     roll = random.randint(1,6)
     dice_sum += roll
-    print(f"You rolled a {roll}")
+    if roll == 1:
+      print(f'You rolled a {roll}! Critical Fail')
+    else:
+      print(f'You rolled a {roll}!')
   print(f"You rolled a total of {dice_sum}")
 
 if __name__== "__main__":
